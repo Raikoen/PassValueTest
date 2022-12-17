58f521228a991f65b5fed8f43937a092fdb638cd
Raikoen <cole.kairu@gmail.com>
2022-12-17 13:59:41 -0600

    Updated style and emulated score calculation

--- Lesson1.js
+++ Lesson1.js
@@ -1,23 +1,37 @@
-// screens/Blog.js
 import React, { Component } from 'react';
-import { Button, View } from 'react-native';
+import { Text, Button, View, StyleSheet } from 'react-native';
+
 class Lesson1 extends Component {
   constructor() {
     super();
   }
   render() {  
+    var countCorrectAnswers = 4;
+    var setScore = countCorrectAnswers * 100;
+    
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-            title="Show My Score"
-            onPress={() => {
-              this.props.navigation.navigate('ScoreDetails', {
-                myScore: 3000,
-              });
-            }}
-          />
+      <View style={styles.container}>
+       <Text style={{padding: 20, fontSize :25, color: 'white', alignSelf: 'center', fontWeight: 'bold'}}>Word to Word!</Text>
+      <Button
+        title="Show My Score"
+        color= "#484085"
+        onPress={() => {
+          this.props.navigation.navigate('ScoreDetails', {
+          myScore: setScore,
+          });
+        }}
+      />
     </View>
     );
   }
 }
 export default Lesson1;
+
+const styles = StyleSheet.create({
+  container :{
+    padding: 30,
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#9d78ec'
+  }
+})
